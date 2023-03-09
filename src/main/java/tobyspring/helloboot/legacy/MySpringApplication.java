838bb4aa7eb2641ce3529be2996988a67907cbd0
김경석 <0xe82de@gmail.com>
2023-03-09 23:39:28 +0900

    update: 섹션 5, 6

--- src/main/java/tobyspring/helloboot/legacy/MySpringApplication.java
+++ src/main/java/tobyspring/helloboot/legacy/MySpringApplication.java
@@ -1,4 +1,4 @@
-package tobyspring.helloboot;
+package tobyspring.helloboot.legacy;
 
 import org.springframework.boot.web.server.WebServer;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
