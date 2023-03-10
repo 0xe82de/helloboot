e63447b903ab555f16c91269586a95718d094042
김경석 <0xe82de@gmail.com>
2023-03-10 23:42:41 +0900

    update: 섹션 7, 8

---
+++ src/main/java/tobyspring/config/autoconfig/ServerProperties.java
@@ -0,0 +1,30 @@
+package tobyspring.config.autoconfig;
+
+import org.springframework.beans.factory.annotation.Value;
+import tobyspring.config.MyConfigurationProperties;
+
+@MyConfigurationProperties(prefix = "server")
+public class ServerProperties {
+
+    @Value("${contextPath:}")
+    String contextPath;
+
+    @Value("${port:8080}")
+    int port;
+
+    public String getContextPath() {
+        return contextPath;
+    }
+
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
+
+    public int getPort() {
+        return port;
+    }
+
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
