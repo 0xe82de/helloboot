680aaf2bdf7f261e46007e5bb811a0b8272aee10
김경석 <0xe82de@gmail.com>
2023-03-11 00:57:20 +0900

    update: 섹션 9

---
+++ src/test/java/tobyspring/helloboot/HelloRepositoryTest.java
@@ -0,0 +1,31 @@
+package tobyspring.helloboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@HellobootTest
+public class HelloRepositoryTest {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    HelloRepository helloRepository;
+
+    @Test
+    void findHelloFailed() {
+        assertThat(helloRepository.findHello("Toby")).isNull();
+    }
+
+    @Test
+    void increaseCount() {
+        helloRepository.increaseCount("Toby");
+        assertThat(helloRepository.countOf("Toby")).isEqualTo(1);
+
+        helloRepository.increaseCount("Toby");
+        assertThat(helloRepository.countOf("Toby")).isEqualTo(2);
+    }
+}
